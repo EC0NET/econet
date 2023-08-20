c3e0e8ae0919f0ca4235656b6bc3e78170963ad5
Praneel Seth <praneelseth@gmail.com>
2023-08-20 10:43:01 -0500

    Footer Test

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import Volunteer from "./components/Volunteer/Volunteer.jsx";
 import Organizations from "./components/Organizations/Organizations.jsx";
 import Support from "./components/Support/Support.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <About/>
-      <Organizations/>
-      <Support/>
+      <style jsx global>{
+      `body {
+             margin: 0px;
+             padding: 0px;
+         }`
+      }</style>
+
+      {/*<Navbar/>*/}
+      {/**<Routes>
+        <Route path="/econet" element={ <About/> } />
+        <Route path="/volunteer" element={ <Volunteer/> } />
+        <Route path="/organizations" element={ <Organizations/> } />
+        <Route path="/support" element={ <Support/> } />
+    </Routes>*/}
+      <Footer/>
     </div>
 
     
